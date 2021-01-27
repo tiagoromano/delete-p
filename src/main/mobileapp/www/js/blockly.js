bb1684c9c7d0556002d4708dadd18d48198d05c3
Root <infra@cronapp.io>
2021-01-27 19:33:44 +0000

    rewe

---
+++ src/main/mobileapp/www/js/blockly.js
@@ -0,0 +1,12 @@
+window.fixedTimeZone = true;
+window.timeZone = 'UTC';
+window.timeZoneOffset = 0;
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.translations = window.translations || {};
+window.translations.localesKeys = ['en_us','pt_br'];
+window.translations.localesRef =  {"en*":"en_us","*":"pt_br","pt*":"pt_br"};
+document.write("<script src='js/blockly/auth/Login.blockly.js?1611775364764'></script>");
+document.write("<script src='js/blockly/auth/Home.blockly.js?1611775364764'></script>");
+document.write("<script src='js/blockly/auth/ChangePassword.blockly.js?1611775364764'></script>");
